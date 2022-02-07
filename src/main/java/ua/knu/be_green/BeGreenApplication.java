c7bd20a76fd4875e28f78aa72908dd401d069c47
Oleksandr Nevmerzhytskyi <oleksandr.nevmerzhytskyi@extreme-idea.com>
2022-02-07 14:27:39 +0200

    init commit

---
+++ src/main/java/ua/knu/be_green/BeGreenApplication.java
@@ -0,0 +1,13 @@
+package ua.knu.be_green;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeGreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeGreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/knu/be_green/BeGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.knu.be_green;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeGreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
